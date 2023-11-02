a6bda3da5897b1eb3f1cb63be65702c58d5f295c
unknown <wenyunyuan@foxmail.com>
2023-11-01 21:51:49 -0700

    project2

--- project2/public/dropdownMenuHandler.js
+++ project2/public/dropdownMenuHandler.js
@@ -1,26 +1,28 @@
-"use strict";
-document.addEventListener('DOMContentLoaded', function() {
-    let navItem = document.querySelector(".nav-links li:nth-child(2) > a"); // Select "About Us" link
-    let dropdown = document.querySelector(".nav-links .dropdown"); // Select the dropdown menu
+(function() {    
+    "use strict";
+    document.addEventListener('DOMContentLoaded', function() {
+        let navItem = document.querySelector(".nav-links li:nth-child(2) > a"); // Select "About Us" link
+        let dropdown = document.querySelector(".nav-links .dropdown"); // Select the dropdown menu
 
-    // Click event listener to toggle visibility of the dropdown menu
-    navItem.addEventListener('click', function(event) {
-        event.preventDefault(); // Prevent default behavior of "About Us" link
-        dropdown.classList.toggle('open'); // Toggle 'open' class
-    });
-
-    // Add an event listener to close the dropdown menu when clicking elsewhere on the page
-    document.addEventListener('click', function(event) {
-        if (!navItem.contains(event.target)) {
-            dropdown.classList.remove('open');
-        }
-    });
-
-    // To enable keyboard navigation, we can listen for the keydown event
-    navItem.addEventListener('keydown', function(event) {
-        if (event.key === "Enter" || event.key === "Space") { // Check if it's the Enter or Space key
-            event.preventDefault(); // Prevent default behavior
+        // Click event listener to toggle visibility of the dropdown menu
+        navItem.addEventListener('click', function(event) {
+            event.preventDefault(); // Prevent default behavior of "About Us" link
             dropdown.classList.toggle('open'); // Toggle 'open' class
-        }
+        });
+
+        // Add an event listener to close the dropdown menu when clicking elsewhere on the page
+        document.addEventListener('click', function(event) {
+            if (!navItem.contains(event.target)) {
+                dropdown.classList.remove('open');
+            }
+        });
+
+        // To enable keyboard navigation, we can listen for the keydown event
+        navItem.addEventListener('keydown', function(event) {
+            if (event.key === "Enter" || event.key === "Space") { // Check if it's the Enter or Space key
+                event.preventDefault(); // Prevent default behavior
+                dropdown.classList.toggle('open'); // Toggle 'open' class
+            }
+        });
     });
-});
+})();
--- project2/public/registrationHandler.js
+++ project2/public/registrationHandler.js
@@ -1,101 +1,102 @@
+(function() {
+    document.addEventListener('DOMContentLoaded', function () {
+        // Modal toggle functionality
+        const subscribeButtons = document.querySelectorAll('.btn-subscribe');
+        const modal = document.getElementById('subscribeModal');
+        const closeBtn = modal.querySelector('.close-btn');
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Modal toggle functionality
-    const subscribeButtons = document.querySelectorAll('.btn-subscribe');
-    const modal = document.getElementById('subscribeModal');
-    const closeBtn = modal.querySelector('.close-btn');
+        subscribeButtons.forEach(btn => {
+            btn.addEventListener('click', function (event) {
+                event.preventDefault();
+                modal.style.display = 'block';
+                resetForm();
+            });
+        });
 
-    subscribeButtons.forEach(btn => {
-        btn.addEventListener('click', function (event) {
-            event.preventDefault();
-            modal.style.display = 'block';
+        closeBtn.onclick = function () {
+            modal.style.display = 'none';
+        };
+
+        window.onclick = function (event) {
+            if (event.target === modal) {
+                modal.style.display = 'none';
+            }
+        };
+
+        document.addEventListener('keydown', function (event) {
+            if (event.key === 'Escape') {
+                modal.style.display = 'none';
+            }
+        });
+
+        // Function to reset the form
+        function resetForm() {
+            const form = document.querySelector('.form');
+            form.reset();
+            const errors = form.querySelectorAll('.error');
+            errors.forEach(err => {
+                err.classList.remove('active');
+            });
+        }
+
+        // Function to validate a field
+        function validateField(fieldId, errorId) {
+            let field = document.getElementById(fieldId);
+            let errorElement = document.getElementById(errorId);
+            let emailFormatErrorElement = document.getElementById('emailFormatError');
+            
+            if (!field.value.trim()) {
+                errorElement.classList.add('active');
+                emailFormatErrorElement.classList.remove('active');
+                return false;
+            } else if (fieldId === 'email' && !field.value.includes('@')) {
+                emailFormatErrorElement.classList.add('active');
+                errorElement.classList.remove('active');
+                return false;
+            } else {
+                emailFormatErrorElement.classList.remove('active');
+            }
+            errorElement.classList.remove('active');
+            return true;
+        }
+
+        document.querySelector('.form').addEventListener('submit', function(event) {
+            let isValid = true;
+
+            // Name validation
+            if (!validateField('name', 'nameError')) {
+                isValid = false;
+            }
+
+            // Email validation
+            if (!validateField('email', 'emailError')) {
+                isValid = false;
+            }
+
+            // Confirm Email validation
+            let emailField = document.getElementById('email');
+            let confirmEmailField = document.getElementById('confirmEmail');
+            if (emailField.value !== confirmEmailField.value) {
+                isValid = false;
+                document.getElementById('confirmEmailError').classList.add('active');
+            } else {
+                document.getElementById('confirmEmailError').classList.remove('active');
+            }
+
+            if (!isValid) {
+                event.preventDefault(); // Stop the form from submitting
+            }
+        });
+
+        // Add 'input' event listener for the name validation
+        document.getElementById('name').addEventListener('input', function() {
+            validateField('name', 'nameError');
+        });
+
+        // Confirm Email is not empty
+        document.getElementById('confirmEmail').addEventListener('input', function() {
+            validateField('confirmEmail', 'confirmEmailError');
         });
     });
+})();
 
-    closeBtn.onclick = function () {
-        modal.style.display = 'none';
-    };
-
-    window.onclick = function (event) {
-        if (event.target === modal) {
-            modal.style.display = 'none';
-        }
-    };
-
-    document.addEventListener('keydown', function (event) {
-        if (event.key === 'Escape') {
-            modal.style.display = 'none';
-        }
-    });
-
-// Function to validate a field
-function validateField(fieldId, errorId, errorMessage) {
-    let field = document.getElementById(fieldId);
-    let errorElement = document.getElementById(errorId);
-    
-    if (!field.value.trim()) {
-        errorElement.innerText = errorMessage;
-        errorElement.classList.add('active');
-        return false;
-    } else if (fieldId === 'email' && !field.value.includes('@')) {
-        errorElement.innerText = "Warning! Email address must include @";
-        errorElement.classList.add('active');
-        return false;
-    } else {
-        errorElement.innerText = "";
-        errorElement.classList.remove('active');
-        return true;
-    }
-}
-
-document.querySelector('.form').addEventListener('submit', function(event) {
-    let isValid = true;
-
-    // Name validation
-    if (!validateField('name', 'nameError', 'Warning! Name is required!')) {
-        isValid = false;
-    }
-
-    // Email validation
-    if (!validateField('email', 'emailError', 'Warning! Email is required!')) {
-        isValid = false;
-    }
-
-    // Confirm Email validation
-    let emailField = document.getElementById('email');
-    let confirmEmailField = document.getElementById('confirmEmail');
-    if (emailField.value !== confirmEmailField.value) {
-        isValid = false;
-        document.getElementById('confirmEmailError').innerText = "Warning！ Emails must match!";
-        document.getElementById('confirmEmailError').classList.add('active');
-    } else {
-        document.getElementById('confirmEmailError').innerText = "";
-        document.getElementById('confirmEmailError').classList.remove('active');
-    }
-
-    if (!isValid) {
-        event.preventDefault(); // Stop the form from submitting
-    }
-});
-
-// Add 'input' event listener for the name validation
-document.getElementById('name').addEventListener('input', function() {
-    validateField('name', 'nameError', 'Warning! Name is required!');
-});
-
-// Confirm Email is not empty
-if (!validateField('confirmEmail', 'confirmEmailError', 'Warning! Confirm Email is required!')) {
-    isValid = false;
-}
-
-// Confirm Email validation
-let emailField = document.getElementById('email');
-let confirmEmailField = document.getElementById('confirmEmail');
-if (emailField.value !== confirmEmailField.value) {
-    isValid = false;
-    document.getElementById('confirmEmailError').innerText = "Warning！ Emails must match!";
-    document.getElementById('confirmEmailError').classList.add('active');
-} else {
-    document.getElementById('confirmEmailError').innerText = "";
-    document.getElementById('confirmEmailError').classList.remove('active');
-}})
